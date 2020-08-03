f6b5d338d814f2b7ee2871a2167b7943bb3a88d9
Ricardo Moura Gimenez <rmgimenez@gmail.com>
2020-08-02 22:59:31 -0300

    correções finais do projeto. agora está inserindo corretamente

--- src/app/orders/orders.component.ts
+++ src/app/orders/orders.component.ts
@@ -11,7 +11,9 @@ export class OrdersComponent implements OnInit {
   coffees = ["Americano", "Flat White", "Cappuccino", "Latte", "Espresso", "Machiato", "Mocha", "Hot Chocolate", "Tea"];
 
   coffeeOrder = [];
+
   addCoffee = coffee => this.coffeeOrder.push(coffee);
+
   removeCoffee = coffee => {
     let index = this.coffeeOrder.indexOf(coffee);
     if (index > -1) this.coffeeOrder.splice(index, 1);
@@ -21,12 +23,10 @@ export class OrdersComponent implements OnInit {
     this.ordersService.form.value.coffeeOrder = this.coffeeOrder;
     let data = this.ordersService.form.value;
 
-    this.ordersService.createCoffeeOrder(data)
-      .then(res => {
-        /*do something here....
-        maybe clear the form or give a success message*/
-      });
-  };
+    this.ordersService.createCoffeeOrder(data).then(res => {
+      /*do something here....maybe clear the form or give a success message*/
+    });
+  }
 
   constructor(public ordersService: OrdersService) { }
 
