c3992953849f4b986d7c1672007e695c790f6699
Ricardo Moura Gimenez <rmgimenez@gmail.com>
2020-08-02 22:09:10 -0300

    cirei os componentes e serviço. colocado no projeto o css do material

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,15 +2,20 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { OrdersComponent } from './orders/orders.component';
+import { OrderListComponent } from './order-list/order-list.component';
+import { OrdersService } from './shared/orders.service';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    OrdersComponent,
+    OrderListComponent
   ],
   imports: [
     BrowserModule
   ],
-  providers: [],
+  providers: [OrdersService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
